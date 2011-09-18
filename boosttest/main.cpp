f714bb07b07eb890220311fb7b4cc6ee595f2895
Per <per.ersson@gmail.com>
2011-09-18 23:25:17 +0200

    Initial Commit

---
+++ boosttest/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  boosttest
+//
+//  Created by Per Ersson on 9/18/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
