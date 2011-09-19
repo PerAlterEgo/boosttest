7f2696e04fd59a7ceb709813696e44373cce236d
Per <per.ersson@gmail.com>
2011-09-19 23:18:32 +0200

    Added worker class for convenience.

--- boosttest/main.cpp
+++ boosttest/main.cpp
@@ -21,19 +21,47 @@ int calculatefib(int n)
 }
 
 
+fib_job::fib_job(int n) : _n(n) {}
+
+int fib_job::calc() const {
+    return calculatefib(_n);
+}
+
+
+template<typename T>
+class worker {
+public:
+    worker(boost::function<T ()> f);
+    ~worker();
+    
+    T get();
+
+private:
+    boost::packaged_task<T> _pack_task;
+    boost::unique_future<T> _future;
+    boost::thread _thread;
+};
+
+template<typename T>
+worker<T>::worker(boost::function<T ()> f) : _pack_task(f), _future(_pack_task.get_future()), _thread(boost::move(_pack_task)) {}
+
+template<typename T>
+worker<T>::~worker() {
+    _thread.join();
+}
+
+template<typename T>
+T worker<T>::get() {
+    return _future.get();
+}
+
 int main (int argc, const char * argv[])
 {
-    boost::packaged_task<int> task(boost::bind<int>(&calculatefib, 12));
-    boost::unique_future<int> future = task.get_future();    
-    boost::thread worker_thread(boost::move(task));
-
-    boost::packaged_task<int> task2(boost::bind<int>(&calculatefib, 12));
-    boost::unique_future<int> future2 = task2.get_future();    
-    boost::thread worker_thread2(boost::move(task2));
-
-    int aa = future.get() + future2.get();
+    worker<int> w1(boost::bind<int>(&calculatefib, 12));
+    worker<int> w2(boost::bind<int>(&calculatefib, 13));
     
-    worker_thread.join();
+    int aa = w1.get() + w2.get();
+
     return 0;
 }
 
