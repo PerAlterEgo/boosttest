1d59be740daba54a66bc555814423849254fe75a
Per <per.ersson@gmail.com>
2011-09-19 22:21:17 +0200

    Simplified use of boost::future

--- boosttest/main.cpp
+++ boosttest/main.cpp
@@ -16,40 +16,22 @@
 
 int calculatefib(int n)
 {
+    boost::this_thread::sleep(boost::posix_time::milliseconds(10)); 
     return n <= 1 ? 1 : calculatefib(n-1)+calculatefib(n-2);
-}
-
-
-class Worker {
-public:
-    Worker(boost::function<int()> f) : _function(f) {}
-    void operator()();
-
-    
-private:
-    boost::function<int()> _function;
-};
-
-void Worker::operator()() {
-    std::cout << "Thread: " << _function() << std::endl;
 }
 
 
 int main (int argc, const char * argv[])
 {
-    boost::function<int()> f = boost::bind<int>(&calculatefib, 12);
-    boost::packaged_task<int> task(f);
-    boost::unique_future<int> future = task.get_future();
-    
-    Worker worker(f);
-    boost::thread worker_thread(worker);
-    
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    
-    task();
-    
-    int aa = future.get();
+    boost::packaged_task<int> task(boost::bind<int>(&calculatefib, 12));
+    boost::unique_future<int> future = task.get_future();    
+    boost::thread worker_thread(boost::move(task));
+
+    boost::packaged_task<int> task2(boost::bind<int>(&calculatefib, 12));
+    boost::unique_future<int> future2 = task2.get_future();    
+    boost::thread worker_thread2(boost::move(task2));
+
+    int aa = future.get() + future2.get();
     
     worker_thread.join();
     return 0;
