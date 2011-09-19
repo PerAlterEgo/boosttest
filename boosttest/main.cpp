c769b82ca812830a3cb1a12c6dc0eae73af0db3c
Per <per.ersson@gmail.com>
2011-09-19 20:20:58 +0200

    Made use of boost::function

--- boosttest/main.cpp
+++ boosttest/main.cpp
@@ -6,22 +6,40 @@
 //  Copyright 2011 __MyCompanyName__. All rights reserved.
 //
 
+#include <vector>
+
 #include <boost/thread/thread.hpp>
+#include <boost/thread/future.hpp>
+#include <boost/bind.hpp>
+#include <boost/function.hpp>
 #include <iostream>
+
+int calculatefib(int d1)
+{
+    return d1;
+}
+
 
 class Worker {
 public:
+    Worker(boost::function<int()> f) : _function(f) {}
     void operator()();
 
+    
+private:
+    boost::function<int()> _function;
 };
 
 void Worker::operator()() {
-    std::cout << "Thread!\n";
+    _function();
 }
+
 
 int main (int argc, const char * argv[])
 {
-    Worker worker;
+    boost::function<int()> f = boost::bind<int>(&calculatefib, 0);
+    
+    Worker worker(f);
     boost::thread worker_thread(worker);
     
     // insert code here...
