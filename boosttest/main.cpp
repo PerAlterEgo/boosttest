e63737a90106109c866a5df0473662610880b035
Per <per.ersson@gmail.com>
2011-09-19 19:36:31 +0200

    Fixed clean compile with boost

--- boosttest/main.cpp
+++ boosttest/main.cpp
@@ -6,13 +6,28 @@
 //  Copyright 2011 __MyCompanyName__. All rights reserved.
 //
 
+#include <boost/thread/thread.hpp>
 #include <iostream>
+
+class Worker {
+public:
+    void operator()();
+
+};
+
+void Worker::operator()() {
+    std::cout << "Thread!\n";
+}
 
 int main (int argc, const char * argv[])
 {
-
+    Worker worker;
+    boost::thread worker_thread(worker);
+    
     // insert code here...
     std::cout << "Hello, World!\n";
+    
+    worker_thread.join();
     return 0;
 }
 
