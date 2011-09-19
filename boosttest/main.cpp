38e6ed5a4f3e18f249f80e73d1109e4ceb70ab3c
Per <per.ersson@gmail.com>
2011-09-19 23:30:31 +0200

    Added more test code.

--- boosttest/main.cpp
+++ boosttest/main.cpp
@@ -14,12 +14,27 @@
 #include <boost/function.hpp>
 #include <iostream>
 
+boost::mutex m_mutex;
+
 int calculatefib(int n)
 {
     boost::this_thread::sleep(boost::posix_time::milliseconds(10)); 
+    {    
+        boost::mutex::scoped_lock l(m_mutex);
+        std::cout << boost::this_thread::get_id() << ": fib(" << n << ")" << std::endl;
+    }
     return n <= 1 ? 1 : calculatefib(n-1)+calculatefib(n-2);
 }
 
+class fib_job {
+public:
+    fib_job(int n);
+    
+    int calc() const;    
+    
+private:
+    int _n;
+};
 
 fib_job::fib_job(int n) : _n(n) {}
 
@@ -47,6 +62,7 @@ worker<T>::worker(boost::function<T ()> f) : _pack_task(f), _future(_pack_task.g
 
 template<typename T>
 worker<T>::~worker() {
+    _future.get();
     _thread.join();
 }
 
@@ -57,11 +73,14 @@ T worker<T>::get() {
 
 int main (int argc, const char * argv[])
 {
-    worker<int> w1(boost::bind<int>(&calculatefib, 12));
-    worker<int> w2(boost::bind<int>(&calculatefib, 13));
+    worker<int> w1(boost::bind<int>(&calculatefib, 7));
+    worker<int> w2(boost::bind<int>(&calculatefib, 8));
+
+    worker<int> w3(boost::bind<int>(&fib_job::calc, boost::shared_ptr<fib_job>(new fib_job(11))));
     
     int aa = w1.get() + w2.get();
 
+    std::cout << "Done" << std::endl;
     return 0;
 }
 
